109d20308ca02592fba437bae27f40389448e5ff
ewang620 <jeremia314@gmail.com>
2025-03-02 11:46:58 +0000

    added command line arguments for difficulty settings

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
+#include <cstdlib>
 #include "/workspaces/SudokuCreater/include/sudoku.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+    
+    int clues = 40; // default clue num
+    if (argc > 1) {
+        clues = std::atoi(argv[1]);
+    }
+    
     SudokuBoard board;
     board.generate(); //complete board
-    
-    int clues = 10;
     board.makePuzzle(clues);
 
     std::cout << "< Sudoku with " << clues << " clues >\n" << std::endl;
